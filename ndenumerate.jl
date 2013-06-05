ee80504462e24955c9e072521cccd6abca655cbd
James Porter <porterjamesj@gmail.com>
2013-06-05 16:18:00 -0400

    works and is twice as fast as enumerate

--- ndenumerate.jl
+++ ndenumerate.jl
@@ -4,40 +4,12 @@ end
 
 ndenumerate(arr) = NDenumerate(arr)
 
-Base.length(nde::NDenumerate) = Base.length(nde.arr)
-Base.start(nde::NDenumerate) = ones(Int,length(size(nde.arr)))
+Base.length(nde::NDenumerate) = length(nde.arr)
+Base.start(nde::NDenumerate) = 1
 
-function Base.next(nde::NDenumerate,state::Array{Int})
-    shape = size(nde.arr)
-    dims = length(shape)
-    
-    #increment the smallest index
-    newstate = copy(state)
-    newstate[1] += 1
-
-    # modulate
-    for i=1:dims
-        if newstate[i] > shape[i]
-            # if the index overflows, mod it around
-            newstate[i] = newstate[i] % shape[i]
-            newstate[i+1] += 1
-        else
-            # if there's no overflow in a an index
-            # no higher-order index has overflow either
-            break
-        end
-    end
-    return (state,nde.arr[state...]),newstate
+function Base.next(nde::NDenumerate, state::Int)
+    sub = ind2sub(size(nde.arr),state)
+    return (sub,nde.arr[state]), state+1
 end
 
-Base.done(nde::NDenumerate,state::Array{Int}) = state == [size(nde.arr)...]
-
-A = rand(100,100);
-
-@elapsed for (i,val)=enumerate(A)
-    println(i,val)
-end
-
-@elapsed for (i,val)=ndenumerate(A)
-    println(i,val)
-end
+Base.done(nde::NDenumerate, state::Int) = state == length(nde)+1

b0ac2cabdfaf5a9f2255aa02b71202290e70c2ad
James Porter <porterjamesj@gmail.com>
2013-06-06 19:14:25 -0400

    worked on life_sets a bit

--- .#life_sets.jl
+++
@@ -1,1 +0,0 @@
-james@xanthos.9992
--- life_sets.jl
+++ life_sets.jl
@@ -1,17 +1,46 @@
 # An attempt to rewrite the game of life using sets rather than arrays
 # (faster for sparse boards)
 
-# One dimensional index => two dimensional index
-function ravel_index(index,shape)
-    xind = index % shape[1]
-    yind = ((index - xind) / shape[1]) + 1
-    return (int(xind), int(yind))
+function get_neighbors(index,shape)
+    i,j = index
+    offsets = filter(x -> x != (0,0),
+                     [(dx,dy) for dx in -1:1, dy in -1:1])
+    neighbors = {}
+    for (dx,dy) in offsets
+        x = i + dx
+        y = j + dy
+        if 1 <= x <= shape[1] &&  1 <= y <= shape[2]
+            push!(neighbors,(x,y))
+        end
+    end
+    return neighbors
 end
 
-# given a twod array, return
+type Board
+    #these should be restricted to the correct tuples and dicts
+    # (need to figure out how to do this)
+    dims::Tuple
+    activecells::Dict
+end
 
-function lifestep(board)
-    shape = size(board)
-    
-    for (x,y) in x=1:shape[1],y=shape[2]
-        
+# board should be a dict 
+function lifestep(board::Board)
+    # this will be a dict whose keys are cells that have any active neighbors
+    # the values are the number of active neighbors
+    cells = {None => None} # how do i create an empty dict?
+    for idx,activecell in enumerate(board.activecells):
+        for neighbor_cell in get_neighbors(ind2sub(idx),board.dims):
+            try
+                cells[neighbor_cell] += 1
+            catch
+                cells[neighbor_cell] = 1
+            end
+        end
+    end
+
+    newboard = board
+
+    # now we have to iterate over all the cells with neighbors to update things
+            
+    end
+end
